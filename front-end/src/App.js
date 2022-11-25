31d656acd0b497665a1f94afbc7443f9f7ae3979
Nuxea <lescieux.am@gmail.com>
2022-11-26 00:09:32 +0100

    add react-bootstrap

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import {Container, Navbar} from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
       <BrowserRouter>
-          <div>
+          <div className="d-flex flex-column site-container">
               <header>
-                  <Link to="/">Amazona</Link>
+                  <Navbar bg="dark" variant="dark">
+                      <Container>
+                          <LinkContainer to="/">
+                              <Navbar.Brand>Amazona</Navbar.Brand>
+                          </LinkContainer>
+                      </Container>
+                  </Navbar>
               </header>
               <main>
-                  <Routes>
-                      <Route path="/product/:slug" element={<ProductScreen />} />
-                      <Route path="/" element={<HomeScreen />} />
-                  </Routes>
+                  <Container>
+                      <Routes>
+                          <Route path="/product/:slug" element={<ProductScreen />} />
+                          <Route path="/" element={<HomeScreen />} />
+                      </Routes>
+                  </Container>
               </main>
+              <footer>
+                  <div className="text-center">
+                      All Rights reserved
+                  </div>
+              </footer>
           </div>
       </BrowserRouter>
   );
