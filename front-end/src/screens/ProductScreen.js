b101a690eedc24f15fbfa8c1aeda9f12ee630155
Nuxea <lescieux.am@gmail.com>
2022-11-26 01:52:08 +0100

    add loading && error messages

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -4,6 +4,9 @@ import axios from "axios";
 import {Badge, Button, Card, Col, ListGroup, Row} from "react-bootstrap";
 import Rating from "../components/Rating";
 import {Helmet} from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import {getError} from "../utils";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -35,7 +38,7 @@ function ProductScreen() {
                 const result = await axios.get(`/api/products/slug/${slug}`);
                 dispatch({type: 'FETCH_SUCCESS', payload: result.data});
             } catch (err) {
-                dispatch({type: 'FETCH_FAIL', payload: err.message });
+                dispatch({type: 'FETCH_FAIL', payload: getError(err) });
             }
         };
         fetchData();
@@ -43,9 +46,11 @@ function ProductScreen() {
 
     return (
         loading ? (
-            <div>Loading...</div>
+            <LoadingBox />
         ) : error ? (
-            <div>{error}</div>
+            <MessageBox variant="danger">
+                {error}
+            </MessageBox>
         ) : (
             <div>
                 <Row>
