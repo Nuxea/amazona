c9319bdd18322f985aea38e5d32d462839380640
Nuxea <lescieux.am@gmail.com>
2022-11-26 03:03:58 +0100

    Complete Add To Cart Functionality

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -1,9 +1,30 @@
 import {Link} from "react-router-dom";
 import {Button, Card} from "react-bootstrap";
 import Rating from "./Rating";
+import {useContext} from "react";
+import axios from "axios";
+import {Store} from "../Store";
 
 function Product(props) {
     const {product} = props;
+
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+    const {cart} = state;
+    const addToCartHandler = async () => {
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const {data} = await axios.get(`/api/products/${product._id}`);
+
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. Product is out of stock');
+            return;
+        }
+
+        ctxDispatch({
+            type: 'CART_ADD_ITEM',
+            payload: {...product, quantity},
+        });
+    };
 
     return (
         <Card>
@@ -16,7 +37,9 @@ function Product(props) {
                 </Link>
                 <Rating rating={product.rating} numReviews={product.numReviews} />
                 <Card.Text>${product.price}</Card.Text>
-                <Button>Add to Cart</Button>
+                <Button onClick={addToCartHandler} variant="primary">
+                    Add to Cart
+                </Button>
             </Card.Body>
         </Card>
     );
