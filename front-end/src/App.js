2cf2742afa99c5aa01a9893d7fef740021570e0c
Nuxea <lescieux.am@gmail.com>
2022-11-26 03:35:57 +0100

    Create Cart Screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import {Badge, Container, Nav, Navbar} from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap';
 import {useContext} from "react";
 import {Store} from "./Store";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
     const { state } = useContext(Store);
@@ -37,6 +38,7 @@ function App() {
                   <Container className="mt-3">
                       <Routes>
                           <Route path="/product/:slug" element={<ProductScreen />} />
+                          <Route path="/cart" element={<CartScreen />} />
                           <Route path="/" element={<HomeScreen />} />
                       </Routes>
                   </Container>
--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useContext, useEffect, useReducer} from "react";
 import axios from "axios";
 import {Badge, Button, Card, Col, ListGroup, Row} from "react-bootstrap";
@@ -23,6 +23,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+    const navigate = useNavigate();
     const params = useParams();
     const {slug} = params;
 
@@ -61,6 +62,7 @@ function ProductScreen() {
             type: 'CART_ADD_ITEM',
             payload: {...product, quantity},
         });
+        navigate('/cart');
     };
 
     return (
